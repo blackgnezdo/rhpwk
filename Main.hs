2ea1b6d9269d7beda5260309f9e2627e5a7f3f85
Matthias Kilian <kili@outback.escape.de>
2012-01-23 15:03:58 +0000

    Simple command line parsing.
    For now, it still can only dump the list of haskell packages (-d) or all
    packages (-d -a).

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
--- Copyright (c) 2010 Matthias Kilian <kili@outback.escape.de>
+-- Copyright (c) 2010,2012 Matthias Kilian <kili@outback.escape.de>
 --
 -- Permission to use, copy, modify, and distribute this software for any
 -- purpose with or without fee is hereby granted, provided that the above
@@ -16,10 +16,40 @@ module Main(main) where
 
 import Database.Sqlports
 import Data.Map (elems)
+import Data.Maybe (fromMaybe)
+import System.Console.GetOpt
+import System.Environment
+
+data Flag = All | Dump deriving (Eq, Show)
+
+options :: [OptDescr Flag]
+options =
+	[ Option ['a']		[]	(NoArg All)	"for -d, dump all package data"
+	, Option ['d']		[]	(NoArg Dump)	"dump package data"
+	]
+
+usage = usageInfo "Usage: rhpwk [-d [-a]]" options
+
+parseOpts :: [String] -> IO ([Flag], [String])
+parseOpts argv =
+	case getOpt Permute options argv of
+		(o, n, []) ->	return (o, n)
+		(_, _, es) ->	ioError (userError usage)
+
 
 main = do
+	argv <- getArgs
+	(flags, args) <- parseOpts argv
+	case args of
+		(_:_) ->	ioError (userError usage)
+		_     ->	return ()
+	case (Dump `elem` flags, All `elem` flags) of
+		(True, False) ->	dumpWith hspkgs
+		(True, True)  ->	dumpWith allpkgs
+		_	      ->	ioError (userError usage)
+
+dumpWith f = do
 	c <- open
-	-- ps <- hspkgs c
-	ps <- allpkgs c
+	ps <- f c
 	close c
 	putStr $ unlines $ map show $ elems ps
